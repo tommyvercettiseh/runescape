860408968528cad2a4275b068468cf797528100d
Hesse <tommyvercettiseh@gmail.com>
2025-12-29 10:45:42 +0100

    10:45

---
+++ _sandbox/test_detect_image.py
@@ -0,0 +1,8 @@
+﻿from vision.image_recognition import detect_image, FOUND_LOG
+
+m = detect_image("WindowsLogo.png", "FullScreen", bot_id=1, method_name="TM_CCOEFF_NORMED", debug=True)
+
+if m:
+    print("gevonden ✅", m)
+else:
+    print("niet gevonden ❌", FOUND_LOG.get(("WindowsLogo.png","FullScreen",1,"TM_CCOEFF_NORMED")))
